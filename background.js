4eeee81da9594b92b7d0b42a68ec0240cb94aee3
xiaomaowei <kawamao@gmail.com>
2026-06-01 16:41:37 +0800

    feat: 移除預設 API 金鑰，強調用戶需自行設定以獲取氣象資訊

--- background.js
+++ background.js
@@ -8,8 +8,8 @@ const LOCATIONS_KEY = "savedLocations";
 const SETTINGS_KEY = "settings";
 
 const DEFAULT_SETTINGS = {
-  apiKey: "CWA-3E823709-E043-4F98-AC95-A1A2986B328F",
-  moenvApiKey: "e905234d-bef9-47c7-9118-8e3f8cdaa846",
+  apiKey: "",
+  moenvApiKey: "",
   cacheTtlMinutes: 60,
   demoMode: false,
   theme: "cute-light-theme"
